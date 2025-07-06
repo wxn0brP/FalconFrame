c015f75102b91bbe921b2a702763ac7a711eb5e1
wxn0brP <toja.filiper@gmail.com>
2025-07-06 08:32:38 +0200

    fix: getMiddlewares

--- src/req.ts
+++ src/req.ts
@@ -143,6 +143,7 @@ function getMiddlewares(middlewares: Middleware[], matchUrl: string, basePath = 
 
         const matches =
             matchUrl === fullPath ||
+            (middleware.use && matchUrl.startsWith(fullPath)) ||
             fullPath.includes(":") ||
             fullPath.includes("*") ||
             matchUrl.startsWith(fullPath + "/");
