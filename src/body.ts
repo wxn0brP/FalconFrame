1fb5670e73703d07cbd2361e81e69a24af9b2143
wxn0brP <viviennevaltheris@gmail.com>
2026-08-04 00:51:59 +0200

    fix: json5

--- src/body.ts
+++ src/body.ts
@@ -5,7 +5,7 @@ export const json: ParseBodyFunction = body => JSON.parse(body);
 export const urlencoded: ParseBodyFunction = body => querystring.parse(body);
 export const yaml: ParseBodyFunction = body => Bun.YAML.parse(body);
 export const toml: ParseBodyFunction = body => Bun.TOML.parse(body);
-export const json5: ParseBodyFunction = body => JSON.parse(body);
+export const json5: ParseBodyFunction = body => Bun.JSON5.parse(body);
 export const xml: ParseBodyFunction = body =>
 	new DOMParser().parseFromString(body, "text/xml");
 export const text: ParseBodyFunction = body => ({
